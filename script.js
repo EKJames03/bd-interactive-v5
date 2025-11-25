e92e6965fcf2e15ebeae8c384df47cf8d9c2e0ac
EKJames03 <126599520+EKJames03@users.noreply.github.com>
2025-11-25 16:58:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
 async function sendData() {
     const data = {};
-    for (let i = 1; i <= 8; i++) {
-        data["bulle_0" + i] = document.getElementById("bulle_0" + i).value.trim();
+    const msg=document.getElementById("statusMsg");
+    for (let i = 1; i <= 12; i++) {
+        let id = "bulle_" + String(i).padStart(2,'0');
+        let el = document.getElementById(id);
+        if (el) data[id] = el.value.trim();
     }
+    msg.innerText="Envoi...";
+    msg.style.color="#007bff";
 
-    const msg = document.getElementById("statusMsg");
-    msg.innerText = "Envoi en cours...";
-    msg.style.color = "#007bff";
-
-    try {
-        let response = await fetch("https://script.google.com/macros/s/AKfycbxTLaEjh_eDleIUMZctXNdMGnKiaJsQhQ3rXvFcUlVrLjqkb_qghvmyYG7VMyibS4mEBA/exec", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
+    try{
+        let response=await fetch(https://script.google.com/macros/s/AKfycby2dzC6sXVFV1d5bIguYT_3K1ea72dGCyj3LsvBsJdecKC67oeQTiR28IYdkG1HCMXZbA/exec,{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(data)
         });
-
-        let text = await response.text();
-        msg.innerText = "Envoyé avec succès ✔";
-        msg.style.color = "green";
-
-    } catch(err) {
-        msg.innerText = "Erreur : " + err;
-        msg.style.color = "red";
+        let text=await response.text();
+        msg.innerText="Envoyé ✔";
+        msg.style.color="green";
+    }catch(err){
+        msg.innerText="Erreur : "+err;
+        msg.style.color="red";
     }
 }
-
